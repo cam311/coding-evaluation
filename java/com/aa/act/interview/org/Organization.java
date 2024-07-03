2c3e62c9c59e75833d5710ff1f14a70222da2ac0
Colby <colby.meyer2000@gmail.com>
2024-07-02 18:08:08 -0700

    Completed Assessment

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,13 +1,20 @@
 package com.aa.act.interview.org;
 
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.Collection;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Random;
 
 public abstract class Organization {
 
     private Position root;
+    private Random rand;
     
     public Organization() {
         root = createOrganization();
+        this.rand = new Random();
     }
     
     protected abstract Position createOrganization();
@@ -20,7 +27,29 @@ public abstract class Organization {
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
+        this.rand = new Random();
+        Queue<Position> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while(queue.peek() != null) {
+            Position current = queue.poll();
+
+            if(current.getTitle().compareTo(title) == 0 && !(current.isFilled())) {
+                current.setEmployee(Optional.of(new Employee(rand.nextInt(1000), person)));
+                return Optional.of(current);
+            }
+            else {
+                Collection<Position> directReports = current.getDirectReports();
+                Iterator<Position> iterator = directReports.iterator();
+
+                while(iterator.hasNext()) {
+                    queue.add(iterator.next());
+                }
+            }
+
+        }
+
         return Optional.empty();
     }
 
